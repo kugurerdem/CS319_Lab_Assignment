09e390c237f3a4447d77625829203d62e19e62a3
Uğur Erdem Seyfi <k.ugurerdem@gmail.com>
2020-04-07 11:46:43 +0300

    another update to main (I realized I had a compiler error)

--- src/Main.java
+++ src/Main.java
@@ -3,29 +3,26 @@ import java.util.Collections;
 
 class Main{
 
-    public static void main(String[] args){
+    public static void main(final String[] args) {
         ArrayList<Integer> ArrayList1, ArrayList2, ArrayList3;
-        
+
         ArrayList1 = new ArrayList<Integer>();
         ArrayList1.add(100);
         ArrayList1.add(500);
         ArrayList1.add(900);
 
-        
         ArrayList2 = new ArrayList<Integer>();
         ArrayList2.add(800);
         ArrayList2.add(600);
         ArrayList2.add(300);
 
-        
         ArrayList3 = new ArrayList<Integer>();
         ArrayList3.add(400);
         ArrayList3.add(700);
         ArrayList3.add(200);
 
-        ArrayOrganizer organizer = new ArrayOrganizer();
-        ArrayList<Integer> sortedList = organizer.concatAndSort( ArrayList1, ArrayList2, ArrayList3);
-
+        final ArrayOrganizer organizer = new ArrayOrganizer();
+        final ArrayList<Integer> sortedList = organizer.concatAndSort(ArrayList1, ArrayList2, ArrayList3);
 
         System.out.print("Result --> ");
         for(int i = 0; i < sortedList.size(); i++){
